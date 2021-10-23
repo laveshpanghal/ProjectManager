e0993c79bb5d8cc3b4f37141b241f10456ba9a1e
laveshpanghal <56184311+laveshpanghal@users.noreply.github.com>
2021-10-23 21:00:50 +0530

    Project-firebase-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
-
+import React from 'react';
+import firestoreDb from "./index";
 
 function App() {
+
+  const click =()=>{
+    console.log('ok')
+
+    firestoreDb.collection('new').doc().set({ 'ok ':'ok'
+    }).then(()=>{
+      alert('success')
+    })
+
+  }
+
+
+
   return (
-    <div className="App">
+    <div className="App" onClick={click}>
     ok
 
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/compat";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeNaPiRX64XdDVenek5PflCsSPs470AZ4",
+    authDomain: "project-manager-5c820.firebaseapp.com",
+    projectId: "project-manager-5c820",
+    storageBucket: "project-manager-5c820.appspot.com",
+    messagingSenderId: "539188572149",
+    appId: "1:539188572149:web:6b538ed5acf582a456babc",
+    measurementId: "G-M7RNJQTH1E"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const firestoreDb =  firebase.firestore();
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +36,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default firestoreDb;
